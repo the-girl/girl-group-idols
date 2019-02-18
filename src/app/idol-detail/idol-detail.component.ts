30ba8fee65899114920774f239cced0cbfff1424
GimYoungPhil <youngphil.gim@gmail.com>
2019-02-18 13:27:45 +0900

    add idol-detail

---
+++ src/app/idol-detail/idol-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-idol-detail',
+  templateUrl: './idol-detail.component.html',
+  styleUrls: ['./idol-detail.component.css']
+})
+export class IdolDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
