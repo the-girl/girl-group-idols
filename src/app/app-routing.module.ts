9dd481e65ce1ae0b266613f82e9bf4fd7871e945
GimYoungPhil <youngphil.gim@gmail.com>
2019-02-18 14:58:18 +0900

    fixed module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { IdolsComponent } from './idols/idols.component';
+import { IdolDetailComponent } from './idol-detail/idol-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'idols', component: IdolsComponent },
+  { path: 'idols/:id', component: IdolDetailComponent },
 ];
 
 @NgModule({
