2c57f4eef8055a1e9bb9442af8fe9d774a51e4ab
GimYoungPhil <youngphil.gim@gmail.com>
2019-02-18 13:27:55 +0900

    add idol

---
+++ src/app/idols/idols.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-idols',
+  templateUrl: './idols.component.html',
+  styleUrls: ['./idols.component.css']
+})
+export class IdolsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
